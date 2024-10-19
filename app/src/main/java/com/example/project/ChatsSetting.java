6466c9f85a189d8cdfc6a5b1e1b866e3a8d8777a
Killg0d <shettytarun2002@gmail.com>
2024-10-19 08:14:41 +0530

    Implemented id for radio buttons

--- app/src/main/java/com/example/project/ChatsSetting.java
+++ app/src/main/java/com/example/project/ChatsSetting.java
@@ -1,12 +1,15 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 
 
 public class ChatsSetting extends BaseSettingActivity {
-
+    RadioGroup theme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +18,16 @@ public class ChatsSetting extends BaseSettingActivity {
         assert actionbar != null;
         actionbar.setTitle("Chats");
         actionbar.setDisplayHomeAsUpEnabled(true);
+        theme = findViewById(R.id.theme);
+        theme.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton b = findViewById(i);
+                if(b.isChecked())
+                {
+                    Toast.makeText(ChatsSetting.this, "b is checked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
