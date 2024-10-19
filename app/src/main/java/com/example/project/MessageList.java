23f72b8db1b53f04854d794bdfafe43a3d8ef1a2
Sinchana <sinchana3.mitmpl2023@learner.manipal.edu>
2024-10-19 10:37:02 +0000

    added listview in MainChat used adapter MessageAdapter.java added new xml file item_chat.xml

--- app/src/main/java/com/example/project/MessageList.java
+++ app/src/main/java/com/example/project/MessageList.java
@@ -4,6 +4,9 @@ public class MessageList {
     private String name;
     private String message;
     private int profilePictureResId;
+    private String chatName;
+    private String lastMessage;
+
 
     public MessageList(String name, String message, int profilePictureResId) {
         this.name = name;
@@ -22,4 +25,15 @@ public class MessageList {
     public int getProfilePictureResId() {
         return profilePictureResId;
     }
+    public String getChatName() {
+        return chatName;
+    }
+
+    // Getter for lastMessage
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+
+
 }
